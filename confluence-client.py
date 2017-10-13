99939ed63c053f405f7a47726b27fa4477fe11f3
Yusuf Iqbal <ysf.iqbl@gmail.com>
2017-10-13 15:10:49 +0530

    Initial Commit

---
+++ confluence-client.py
@@ -0,0 +1,67 @@
+"""
+This client will/can/should be changed to a CLI eventually
+This client class is used to consume confluence.py there by leaving confluence.py
+to handle API calls only.
+
+This class may later be converted to a CLI
+"""
+import sys, json
+from confluence import Confluence
+
+class ConfluenceClient(object):
+
+	def __init__(self, url, username, password):
+		self.confluence = Confluence(url, username, password)
+
+	"""
+	Get a array of all page titles (including children) in a space.	
+	"""
+	def get_page_names(self, space_key):
+		has_next = True	
+		page_names = []
+		start = 0
+		limit = 25
+		while has_next is True:
+			pages = self.confluence.get_pages(space_key, start, limit).json()
+			results = pages['results']
+			for page in results:
+				page_names.append(page['title'])
+			if 'next' in pages['_links']:
+				start = pages['start'] + pages['limit']
+			else:
+				has_next = False
+		return page_names
+	
+	
+	"""
+	Create an empty page in the give Confluence space
+	"""
+	def create_empty_page(self, space_key, title, parent_page_id=None):
+		if parent_page_id is not None:
+			ancestors = [{ 'id': parent_page_id }]
+		else:
+			ancestors = None		
+
+		return self.confluence.create_page(space_key, title, ancestors, '')
+
+
+	"""
+	Write content to file
+	"""
+	def write_to_file(filepath, content):
+		outfile = open(filepath, 'w')
+		outfile.write(content)
+		outfile.close()
+	
+"""
+Change this main program to use the API.
+"""
+if __name__ == '__main__':
+	with open(sys.argv[1], 'r') as config:
+		conf = json.load(config)	 
+		url = conf['url']
+		username = conf['username']
+		password = conf['password']
+		client = ConfluenceClient(url, username, password)
+		print(client.confluence.get_space('SPACEKEY').json())
+	
---
+++ confluence.py
@@ -0,0 +1,74 @@
+"""
+This class is used to consume the Confluence API. It has only been tested with Confluence 6.0.3
+"""
+import requests, json
+
+class Confluence(object):
+	def __init__(self, url, username, password):
+		self.url = url
+		self.api_url = url + '/rest/api'
+		self.username = username
+		self.password = password
+
+	"""
+	Print JSON response to stdout
+	"""
+	def printResponse(r):
+        	print '{} {}\n'.format(json.dumps(r.json(), sort_keys=True, indent=4, separators=(',', ': ')), r)
+
+
+	"""
+	GET /rest/api//space/<space_key>
+	"""
+	def get_space(self, space_key):
+		space_get_url = '{0}/space/{1}'.format(self.api_url, space_key)
+		return requests.get(space_get_url, auth=(self.username, self.password))
+	
+	"""
+	POST /rest/api/space
+	"""
+	def create_space(self, space_key, space_name):
+		data = {
+			'key': space_key,
+			'name': space_name
+		}
+		create_space_url = '{0}/space'.format(self.api_url)
+		return requests.post(create_space_url, data=json.dumps(data), auth=(self.username, self.password), headers=({'Content-Type': 'application/json'}))
+	
+	"""
+	DELETE /rest/api/space/<space_key>
+	"""
+	def delete_space(self, space_key):
+		space_delete__url = '{0}/space/{1}'.format(self.api_url, space_key)
+		return requests.delete(space_delete_url, auth=(self.username, self.password))
+	
+	"""
+	POST /rest/api/content
+	ancestors e.g [{'id': parentPageId}]
+	"""
+	def create_page(self, space_key, title, ancestors, content):
+		create_page_url = '{0}/content'.format(self.api_url)
+		data = {
+                '	type': 'page',
+                	'title': title,
+                	'space': {'key': space_key},
+                	'body': {
+                        	'storage': {
+                                	'value': content,
+                                	'representation': 'storage'
+                        	}
+                	}
+        	}
+		
+		if ancestors != None:
+			data['ancestors'] = ancestors
+		return requests.post(create_page_url, data=json.dumps(data), auth=(self.username, self.password), headers=({'Content-Type': 'application/json'}))
+	
+	
+	
+	"""
+	GET /rest/api/content?spaceKey=<space_key>&start=<start>&limit=<limit>
+	"""	
+	def get_pages(self, space_key, start=0, limit=25):
+		pages_get_url = '{0}/content?spaceKey={1}&start={2}&limit={3}'.format(self.api_url, space_key, start, limit)
+		return requests.get(pages_get_url, auth=(self.username, self.password))
